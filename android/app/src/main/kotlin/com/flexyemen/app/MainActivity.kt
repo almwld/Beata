b5a9c009fbd1af84f3d8a1bd27599ee12ff61e4e
almwld <your-email@example.com>
2026-03-27 22:39:27 +0300

    chore: remove android folder to let CI regenerate it

--- android/app/src/main/kotlin/com/flexyemen/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.flexyemen.app
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
